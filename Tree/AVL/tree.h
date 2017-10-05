66d8b8ff5cca3cb9c8e0c11f71f976158e8df349
LockLee <627311250@qq.com>
2017-10-05 20:50:38 +0800

    Update tree.h

--- Tree/AVL/tree.h
+++ Tree/AVL/tree.h
@@ -1,19 +1,38 @@
-#ifndef _Head_Tree_
-#define _Head_Tree_
+#ifndef _AVL_TREE_H
+#define _AVL_TREE_H
 
+//定义结构体节点
 struct AVLNode;
+
 typedef struct AVLNode Node;
 typedef struct AVLNode *Position;
 typedef struct AVLNode *AvlTree;
 
+//前序遍历AVL树
+void Preorder(AvlTree );
+
+//中序遍历AVL树
 void Inorder(AvlTree );
-AvlTree MakeEmpty (AvlTree );
+
+//销毁AVL树
+void DestroyTree(AvlTree );
+
+//在AVL树中查找
 Position Find(int ,AvlTree );
+
+//查找AVL树中的最小值
 Position FindMin(AvlTree );
+
+//查找AVL树的最大值
 Position FindMax(AvlTree );
+
+//将元素插入AVL树中
 AvlTree Insert(int ,AvlTree );
+
+//在AVL树中删除元素
 AvlTree Delete(int ,AvlTree);
 
+//AVL数据结构
 struct AVLNode
 {
 	int Value;
