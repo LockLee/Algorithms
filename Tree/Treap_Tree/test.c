f6138365ffda5e79a5fd611bae5336136f7c02d3
LockLee <627311250@qq.com>
2017-10-26 23:06:26 +0800

    Update test.c

--- Tree/Treap_Tree/test.c
+++ Tree/Treap_Tree/test.c
@@ -10,6 +10,7 @@
 #include <limits.h>
 #include "treap.h"
 
+//求数组大小
 #define TBL_SIZE(a) ( (sizeof(a)) / (sizeof(a[0])) )
 
 int main()
@@ -19,20 +20,26 @@ int main()
 	int A[] = {1,2,3,4,5,5,6,7,7,8};
 	treapTree T = NULL;	
 	
+	//随机数种子
 	srand((unsigned)time(NULL));
 		
+	//将数组 A 中元素插入Treap 中
 	N = TBL_SIZE(A);
 	for(i = 0; i < N; i++)
 		T = Insert(T, A[i]);
 	
+	//打印Treap 信息
 	PrintTree(T, T->value, 0);
 	
+	//在Treap 中删除元素 6
 	T = Delete( T, 6);
 	
 	PrintTree(T, T->value, 0);
 	
+	//找到Treap 中第 6 个元素
 	printf("Find 6th : %d\n",FindKth(T, 6)->value);
 		
+	//计算Treap 中小于 6 的元素个数
 	printf("Less 6 num: %d\n", LessX(T, 6, 0));
 		
 	return 0;
