68519b351539f527c77614999ee1ed6e49e052f9
LockLee <627311250@qq.com>
2017-11-08 16:18:17 +0800

    Update cut_rod.c

--- Dynamic_Programming/Rod_Cutting/cut_rod.c
+++ Dynamic_Programming/Rod_Cutting/cut_rod.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/*
+   Given a rod of length n inches and a table of prices pi for i = 1、2、3 . . . n, 
+   determine the maximum revenue rn obtainable by cutting up the rod and selling the pieces. 
+
+   Note that if the price pn for a rod of length n is large enough, an optimal solution may require no cutting at all.
+*/
+
+/*
+ *自底向上计算最优切割方案
+ *
+ *参数 p: 不同英寸钢条价格
+ *     n: 钢条长度
+ *
+ *打印输出长度为 n 的钢条的最大利润并输出一个最优切割方案
+ */
 void Bottom_Up_Cut_Rod(int p[], int n)
 {
 	int i, j;
 
 	int q;
 
+	//保存不同长度钢条的最大利润
 	int r[n + 1];
+	//保存不同长度钢条的最近一次切割方案
 	int s[n + 1];
 	
+	//长度为 0 的钢条的最大利润和最近一次切割都为 0
 	r[0] = 0;
 	s[0] = 0;
 	
@@ -30,7 +48,9 @@ void Bottom_Up_Cut_Rod(int p[], int n)
 	
 	printf("The length of %d max revenue is:%d\n",n, r[n]);
 	
+	//打印输出最大利润
 	printf("An optimal solution is:");
+	//打印输出切割方案
 	while(n > 0)
 	{
 		printf("%d   ",s[n]);
@@ -43,8 +63,9 @@ int main()
 {
 	int i;
 	int p[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+	int length = sizeof(p)/sizeof(p[0]);
 	
-	for(i = 1; i <=10; i++)
+	for(i = 1; i < lenght; i++)
 		Bottom_Up_Cut_Rod(p, i);
 	
 }
