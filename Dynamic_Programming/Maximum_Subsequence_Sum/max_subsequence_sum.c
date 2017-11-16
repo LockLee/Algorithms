c586630e1e1e9a4d8a53b2dc15824ee7fd2b4749
LockLee <627311250@qq.com>
2017-11-16 20:29:48 +0800

    Update max_subsequence_sum.c

--- Dynamic_Programming/Maximum_Subsequence_Sum/max_subsequence_sum.c
+++ Dynamic_Programming/Maximum_Subsequence_Sum/max_subsequence_sum.c
@@ -28,7 +28,7 @@ void Max_Subsequence_Sum(int arr[], int length)
 	//该数组保存以当前 i 结尾的最大子序列和
 	int sumhere[length];
 
-	//在每次循环之前，maxendinghere 包含了截止与位置 i-1 的最大子序列的值，
+	//在每次循环之前，maxendinghere 包含了截止于位置 i-1 的最大子序列的值，
 	//循环开始后，赋值语句修改它以包含截止与位置 i 的最大子序列和，
 	//当它变为负值时，就将它重新设为 0 （因为截止与 i 的最大子序列现在是空序列了）
 	for(i = 0; i < length; i++)
