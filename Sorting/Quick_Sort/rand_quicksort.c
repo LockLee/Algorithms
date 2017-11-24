ddcd117d507f0603289d438038b68f78458eca67
LockLee <627311250@qq.com>
2017-11-24 10:51:51 +0800

    Update rand_quicksort.c

--- Sorting/Quick_Sort/rand_quicksort.c
+++ Sorting/Quick_Sort/rand_quicksort.c
@@ -1,3 +1,9 @@
+/*
+ *NAME: A RANDOMIZED VERSION OF QUICKSORT(EXPECTED RUNNING TIME:O(nlgn))
+ *TIME: 2017.11.24
+ *AUTHOR: LS
+ */ 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -21,45 +27,52 @@ int main()
 	printf("\n");
 }
 
-
+//partition()函数将数子组 A[p,...,r] 分为两部分，一部分大于主元，一部分小于主元
+//参数 A[] : 待排序数组
+//     p   : 待排序子数组开始下标
+//     r   : 待排序子数组结束下标
 int rand_partition(int *A, int p,int r)				
 {
 	int i,j,randi;
 	i = p - 1;
 	int pivot;
 	
+	//随机产生一个[p,r]之间的一个随机数（包括 p 和 r）
 	srand((unsigned int)time(NULL));	
 	randi = (rand()%(r-p+1))+p;
-	printf("rand number = %d\n",randi);
+	//printf("rand number = %d\n",randi);
 	
+	//将产生的随机主元与最后一个元素（A[r]）交换
 	swap(&A[r], &A[randi]);
 	
-	pivot = A[r];						//选取数组最后一个元素作为主元
-	for(j = p;j < r;j++)					//遍历数组
+	pivot = A[r];						
+	for(j = p;j < r;j++)					
 	{
 		if(A[j] <= pivot)
 		{
 			i++;
-			swap(&A[i], &A[j]);			//交换函数
+			swap(&A[i], &A[j]);			
 		}	
 	}
 	swap(&A[r], &A[i+1]);
 	return i+1;
 }
 
-
-
-void rand_quicksort(int *A, int p, int r)				//quicksort()函数实现对数组的递归排序
+//quicksort()函数实现对数组的递归排序
+//参数 A[] : 待排序数组
+//     p   : 待排序子数组开始下标
+//     r   : 待排序子数组结束下标
+void rand_quicksort(int *A, int p, int r)				
 {	
 	int i;
 	int q;
 	if(p < r)
 	{
-		q = rand_partition(A,p,r);				//q 为主元位置
+		q = rand_partition(A,p,r);				
 		for(i = 0; i < 8; i++)
 			printf("%d   ",A[i]);
 		printf("\n\n");
-		rand_quicksort(A,p,q-1);				//递归调用quicksort()函数
+		rand_quicksort(A,p,q-1);				
 		rand_quicksort(A,q+1,r);	
 	}
 }
