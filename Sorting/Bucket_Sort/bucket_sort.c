5b59a93e400961e36269bfd70c2b60323e79a352
LockLee <627311250@qq.com>
2017-11-25 19:54:10 +0800

    Update bucket_sort.c

--- Sorting/Bucket_Sort/bucket_sort.c
+++ Sorting/Bucket_Sort/bucket_sort.c
@@ -1,9 +1,28 @@
+/*
+ *NAME:BUCKET-SORT
+ *TIME:2017.11.25
+ *AUTHOR:LS
+ */
+
+/*
+The Best-case performance O(n).
+ 
+The Worst-case performance O(n2).
+
+The Average performance O(n).
+
+The Bucket Sort is Stability.
+
+The Worst-case space complexity О(n) total, O(n) auxiliary.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <limits.h>
 
+//定义结构体节点类型
 struct node;
 typedef struct node Node;
 typedef struct node *ptrNode;
@@ -44,13 +63,21 @@ int main()
 
 }
 
+/*
+ *将元素依次放入相应桶中，并按照非递减序排列
+ *
+ *函数参数 X：需要放入相应桶中的元素值
+ *      *pB：元素放入对应桶的头指针
+ *
+ */
 void insert_sort(int X, ptrNode *pB)
 {
 	ptrNode p,prev,ptmp;
 	p = NULL;
 	prev = NULL;
 	ptmp = NULL;
-	
+
+	//申请一个节点空间
 	ptmp = (ptrNode)malloc(sizeof(Node));
 	if(ptmp == NULL)
 	{
@@ -60,6 +87,8 @@ void insert_sort(int X, ptrNode *pB)
 	ptmp->value = X;
 	ptmp->next = NULL;
 	
+	//如果该桶为空，直接将该元素值放入桶中
+	//否则，按照非递减序放入该桶中
 	if(*pB == NULL)
 		*pB = ptmp;
 	else
@@ -71,6 +100,8 @@ void insert_sort(int X, ptrNode *pB)
 			prev = p;
 			p = p->next;
 		}
+		//此行很关键
+		//主要没有头节点，需要判断是不是将元素值插在第一个位置
 		if(prev == *pB && X < prev->value)
 		{
 			ptmp->next = prev;
@@ -84,6 +115,14 @@ void insert_sort(int X, ptrNode *pB)
 	}	
 }
 
+/*
+ *桶排序主要例程
+ *
+ *参数 A[]：待排序数组
+ *       n：数组大小
+ *    maxn：数组中最大元素值
+ *
+ */
 void bucket_sort(int A[], int n, int maxn)
 {
 	int i, j, k;
@@ -97,6 +136,7 @@ void bucket_sort(int A[], int n, int maxn)
 	for(j = 0; j <= n; j++)
 		Bucket[j] = NULL;
 	
+	//将数组中元素依次放入相应桶中
 	for(i = 0; i < n; i++)
 	{
 		insert_sort(A[i], &Bucket[A[i]/k]);
@@ -116,7 +156,7 @@ void bucket_sort(int A[], int n, int maxn)
 		}
 	}
 */
-	
+	//将桶中元素依次取出
 	for(i = 0,j = 0; j <= n; j++)
 	{
 		if(Bucket[j] != NULL)
