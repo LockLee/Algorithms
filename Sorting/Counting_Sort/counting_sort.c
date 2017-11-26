faa61b448fc3f56e32cd1575bf47141ee762d2ae
LockLee <627311250@qq.com>
2017-11-26 15:16:21 +0800

    Update counting_sort.c

--- Sorting/Counting_Sort/counting_sort.c
+++ Sorting/Counting_Sort/counting_sort.c
@@ -1,7 +1,20 @@
 /*
- *NAME:COUNTING SORT(O(n))
+ *NAME:COUNTING SORT(O(n + k))
  *TIME:2016.10.2	
  *AUTHOR:LS
+ * *UUPDATE:2017.11.26,Add some comments.
+ */
+
+/*
+The Best-case performance O(n + k), Where k is the range of integers.
+
+The Worst-case performance O(n + k).
+
+The Average performance O(n + k).
+
+The Counting Sort is Stability.
+
+The Worst case space complexity is O(n + k) auxiliary.
 */
 
 #include <stdio.h>
@@ -23,8 +36,13 @@ int main()
         {
                 scanf("%d",&A[i]);
         }
-	k = maximum(A, n) + 1;					//maximum()函数计算A[]数组最大值，k表示A[]数组的范围，从0开始
+	
+	//maximum()函数计算A[]数组最大值，k表示A[]数组的范围，从0开始
+	k = maximum(A, n) + 1;					
+	
+	//计数排序
 	countingSort(A, n, k);
+	
 	for(i = 0;i < n;i++)
 	{
 		printf("%d	",A[i]);
@@ -33,7 +51,8 @@ int main()
         return 0;
 }
 
-int maximum(int *A,int n)					 //maximum()函数计算A[]数组最大值
+//maximum()函数计算A[]数组最大值
+int maximum(int *A,int n)					 
 {
 	int i,max;
 	max = A[0];
@@ -45,27 +64,47 @@ int maximum(int *A,int n)					 //maximum()函数计算A[]数组最大值
 	return max;		
 }
 
+/*
+ *计数排序
+ *
+ *参数  A：待排序数组
+ *      n：数组大小
+ *      k：数组最大元素值
+ */
 void countingSort(int *A,int n,int k)
 {
 	int i,j;
-	int B[n],C[k];						//B[]数组用来排序，C[]数组用来计数
-	for(i = 0;i < k;i++)					//初始化C[]为0
+	
+	//B[]数组用来排序，C[]数组用来计数
+	int B[n],C[k];			
+	
+	//初始化C[]为0
+	for(i = 0;i < k;i++)					
 	{
 		C[i] = 0;
 	}
-	for(j = 0;j < n;j++)					//统计元素等于A[j]的元素个数
+	
+	//统计元素等于A[j]的元素个数
+	for(j = 0;j < n;j++)					
 	{
 		C[A[j]]++;		
 	}
-	for(i = 1;i < k;i++)					//统计有多少元素小于等于A[i]的
+	
+	//统计有多少元素小于等于A[i]的
+	for(i = 1;i < k;i++)					
 	{
 		C[i] += C[i-1];
 	}
-	for(j = n-1;j >= 0;j--)					//把每个元素A[j]放到B[]数组中的正确位置
+	
+	//把每个元素A[j]放到B[]数组中的正确位置
+	for(j = n-1;j >= 0;j--)					
 	{
-		B[--C[A[j]]] = A[j];				//数组B[]下标从0开始，所以A[j]的正确位置是C[A[j]] - 1
-		//C[A[j]]--;
+		//数组B[]下标从0开始，所以A[j]的正确位置是C[A[j]] - 1
+		B[--C[A[j]]] = A[j];				
+		
 	}
-	for(j = 0;j < n;j++)					//把数组B[]复制给数组A[]
+	
+	//把数组B[]复制给数组A[]
+	for(j = 0;j < n;j++)					
 		A[j] = B[j];	
 }
