0f879d8729884e926f8b6453df2d2104d50e0bdf
LockLee <627311250@qq.com>
2017-12-05 15:32:47 +0800

    Update maximum_subarray_sum.c

--- Divide_And_Conquer/Maximum_Subarray_Sum/maximum_subarray_sum.c
+++ Divide_And_Conquer/Maximum_Subarray_Sum/maximum_subarray_sum.c
@@ -1,3 +1,25 @@
+/*
+ *NAME:MAXIMUM-SUBARRAY-SUM(O(nlgn))
+ *TIME:2017.12.5
+ *AUTHOR:LS
+ */
+
+/*
+ PROBLEM:
+ You are given a one dimensional array that may contain both positive and negative integers, 
+ find the sum of contiguous subarray of numbers which has the largest sum.
+ For example, if the given array is {-2, -5, 6, -2, -3, 1, 5, -6}, then the maximum subarray 
+ sum is 7 (see highlighted elements).
+ 
+ Using Divide and Conquer approach, we can find the maximum subarray sum in O(nLogn) time. 
+ Following is the Divide and Conquer algorithm.
+ 1) Divide the given array in two halves
+ 2) Return the maximum of following three
+ ….a) Maximum subarray sum in left half (Make a recursive call)
+ ….b) Maximum subarray sum in right half (Make a recursive call)
+ ….c) Maximum subarray sum such that the subarray crosses the midpoint
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
